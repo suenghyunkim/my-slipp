5d60e2541437f4606da43bb28bf554c6e0f62b2e
SeungHyun92 <tmdgusdl92@gmail.com>
2019-09-20 06:58:17 +0900

    로그인 사용자에 한해 개인정보 수정 가능하도록 구현

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -11,13 +11,17 @@ public class User {
 	@GeneratedValue
 	private Long id;
 	
-	@Column(nullable=false,length=20)
+	@Column(nullable=false,length=20,unique=true)
 	private String userId;
 	
 	private String password;
 	private String name;
 	private String email;
 
+	public Long getId() {
+		return id;
+	}
+	
 	public String getUserId() {
 		return userId;
 	}
